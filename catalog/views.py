66c70d2698faa9981023fdd01316dd9f3272c87d
Evg <nrmPzn@mail.ru>
2021-02-04 23:07:33 +0300

    file load

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.POST['name'], request.FILES['aud'])
+    return render(request, 'index.html')
+
+
+def handle_uploaded_file(filename, userfile):
+    filename += '.mp3'
+    file = open(filename, 'wb+')
+    for chunk in userfile.chunks():
+        file.write(chunk)
