10b0f444abb56ffd2e6f0c864ef5a1e09c3035e6
anna <luribina@gmail.com>
2021-02-05 19:51:19 +0300

    login and logout

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, signup
+from .views import index, signup, user_login, user_logout
 
 urlpatterns = [
-    path('',index),
-    path('signup/',signup),
-    # path('login/',)
+    path('', index),
+    path('signup/', signup),
+    path('login/', user_login, name='user_login'),
+    path('logout/', user_logout)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
 import os
 
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-
+from django.contrib.auth import login, authenticate, logout
+from django.http import HttpResponse
 from catalog.forms import SignUpForm
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
     if request.method == 'POST':
@@ -12,19 +14,20 @@ def index(request):
 
 
 def handle_uploaded_file(filename, userfile):
-    newFile = './audioInputFiles/'+filename+'.mp3'
+    newFile = './audioInputFiles/' + filename + '.mp3'
     file = open(newFile, 'wb+')
     for chunk in userfile.chunks():
         file.write(chunk)
 
+
 def signup(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()
             user.profile.email = form.cleaned_data.get('email')
-            user.profile.dir_path = './'+ form.cleaned_data.get('username')
+            user.profile.dir_path = './' + form.cleaned_data.get('username')
             user.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=user.username, password=raw_password)
@@ -32,4 +35,24 @@ def signup(request):
             return redirect('/')
     else:
         form = SignUpForm()
-    return render(request,'signup.html',{'form':form})
+    return render(request, 'signup.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            return HttpResponse("Invalid Login or password")
+    else:
+        return render(request, 'login.html', {})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('/')
