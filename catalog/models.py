9e41358774aa5e64da9f8300991e5007f878cf9a
anna <luribina@gmail.com>
2021-02-05 16:02:26 +0300

    sign up form

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class CustomUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    email = models.EmailField(max_length=100, blank=True)
+    dir_path = models.TextField(max_length=100, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user(sender, instance, created, **kwargs):
+    if created:
+        CustomUser.objects.create(user=instance)
+    instance.profile.save()
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,9 @@
 import os
 
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 
-
-# Create your views here.
+from catalog.forms import SignUpForm
 
 def index(request):
     if request.method == 'POST':
@@ -16,3 +16,20 @@ def handle_uploaded_file(filename, userfile):
     file = open(newFile, 'wb+')
     for chunk in userfile.chunks():
         file.write(chunk)
+
+def signup(request):
+    if request.method=='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.email = form.cleaned_data.get('email')
+            user.profile.dir_path = './'+ form.cleaned_data.get('username')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request,'signup.html',{'form':form})
