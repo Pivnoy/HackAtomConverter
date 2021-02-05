fbd4f50f6247985493acd53e654b0ee8e479f694
Pivnoy <evgenij_makarev20021@mail.ru>
2021-02-06 02:24:03 +0300

    nwe

---
+++ Process/link.py
@@ -0,0 +1,24 @@
+import psycopg2
+import time
+from Algo.lang_process import text_processing
+
+con = psycopg2.connect(
+    database="hackatom",
+    user="lubovmakareva",
+    password="",
+    host="127.0.0.1",
+    port="5432"
+)
+
+cursor = con.cursor()
+
+while True:
+    cursor.execute("SELECT * FROM TAB WHERE proc=FALSE")
+    rows = cursor.fetchall()
+    for row in rows:
+        cursor.execute("UPDATE TAB SET proc = TRUE WHERE id = %s", [row[0]])
+        # all_text = text_processing(row)
+        all_text = 'Pidarasy blyad'
+        # TODO: Database
+        cursor.execute("INSERT INTO TEXT VALUES (%s, %s)", (row[0], all_text))
+        con.commit()
